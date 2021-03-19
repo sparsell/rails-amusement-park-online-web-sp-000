5699aae7b62e6503e6ae9f39ed4c642deedf78dd
Sonja Parsell <hello@sparsell.com>
2021-03-19 12:01:45 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,11 +11,21 @@ class SessionsController < ApplicationController
             # binding.pry 
             session[:user_id] = @user.id 
             redirect_to user_path(@user)
+            # binding.pry
         else
             flash[:message] = "Login not successful. Please try again."
             render 'new'
         end
     end
+
+#     def create
+#     @user = User.find_by(name: params[:user][:name])
+#     if @user
+#       if @user.authenticate(params[:user][:password])
+#         session[:user_id] = @user.id
+#       end
+#     end
+#   end
 
     def destroy
         session.delete :user_id
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post 'signup', to: 'users#create'
   get 'signin', to: 'sessions#new'
   post 'signin', to: 'sessions#create'
+  post '/sessions', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
   
 
